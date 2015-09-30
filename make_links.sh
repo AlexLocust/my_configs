c6be070a1a8fc6fc2fb90a4da6431a58c69e8d3b
Alexey Schukarev <aschukarev@npoprogress.com>
2015-09-30 17:24:51 +0300

    Powerline fonts (from https://github.com/powerline/fonts)

--- make_links.sh
+++ make_links.sh
@@ -6,7 +6,7 @@ ln -s ~/.my_configs/xfce4/ ~/.config/xfce4
 ln -s ~/.my_configs/xfce4-session/ ~/.config/xfce4-session
 ln -s ~/.my_configs/.zshrc ~/.zshrc
 ln -s ~/.my_configs/.Xresources ~/.Xresources
-
+ln -s ~/.my_configs/.fonts ~/.fonts
 
 
 
