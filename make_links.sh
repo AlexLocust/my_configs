49eb5c8c6c248a4d2393b6d24460db257ed0510a
Alexey Schukarev <aschukarev@npoprogress.com>
2015-09-30 17:59:25 +0300

    Oh My ZSH configs from https://github.com/robbyrussell/oh-my-zsh

--- make_links.sh
+++ make_links.sh
@@ -4,10 +4,13 @@ ln -s ~/.my_configs/.tmux.conf ~/.tmux.conf
 ln -s ~/.my_configs/fontconfig/fonts.conf ~/.fonts.conf
 ln -s ~/.my_configs/xfce4/ ~/.config/xfce4
 ln -s ~/.my_configs/xfce4-session/ ~/.config/xfce4-session
-ln -s ~/.my_configs/.zshrc ~/.zshrc
 ln -s ~/.my_configs/.Xresources ~/.Xresources
 ln -s ~/.my_configs/.fonts ~/.fonts
 
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+rm ~/.zshrc
+ln -s ~/.my_configs/.zshrc ~/.zshrc
 
 
 
