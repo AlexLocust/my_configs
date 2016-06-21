dfa1f4e639a77e197b00550dc1f1cb22fafd767c
Alexey Shchukarev <aschukarev@gmail.com>
2016-06-21 22:11:19 +0300

    Vundle in submodules, improved installation script

--- make_links.sh
+++ make_links.sh
@@ -1,16 +1,20 @@
-ln -s ~/.my_configs/.vim/.vimrc ~/.vimrc
-ln -s ~/.my_configs/.vim/ ~/.vim
-ln -s ~/.my_configs/.tmux.conf ~/.tmux.conf
-ln -s ~/.my_configs/fontconfig/fonts.conf ~/.fonts.conf
-ln -s ~/.my_configs/xfce4/ ~/.config/xfce4
-ln -s ~/.my_configs/xfce4-session/ ~/.config/xfce4-session
-ln -s ~/.my_configs/.Xresources ~/.Xresources
-ln -s ~/.my_configs/.fonts ~/.fonts
+sudo apt-get install aptitude vim-nox-py2 mc cmake python-dev python3-dev ffmpeg xclip
+
+git submodule update --init --recursive
+
+ln -s ~/my_configs/.vim/.vimrc ~/.vimrc
+ln -s ~/my_configs/.vim/ ~/.vim
+ln -s ~/my_configs/.tmux.conf ~/.tmux.conf
+# install VIM plugins throught vundle
+vim +PluginInstall +qall
+#ln -s ~/.my_configs/fontconfig/fonts.conf ~/.fonts.conf
+#ln -s ~/.my_configs/xfce4/ ~/.config/xfce4
+#ln -s ~/.my_configs/xfce4-session/ ~/.config/xfce4-session
+#ln -s ~/.my_configs/.Xresources ~/.Xresources
+#ln -s ~/.my_configs/.fonts ~/.fonts
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 rm ~/.zshrc
-ln -s ~/.my_configs/.zshrc ~/.zshrc
-
-
+ln -s ~/my_configs/.zshrc ~/.zshrc
 
