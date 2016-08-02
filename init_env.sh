5a522a3e12d5002ccf49bfbbc40cc2de11a34352
Alexey Schukarev <aschukarev@npoprogress.com>
2016-08-02 11:46:47 +0300

    Updated init script

--- init_env.sh
+++ init_env.sh
@@ -1,18 +1,21 @@
 sudo apt-get install aptitude vim-nox-py2 mc cmake python-dev python3-dev ffmpeg xclip
-sudo virtualenvwrapper htop ninja-build
+sudo apt-get install virtualenvwrapper htop ninja-build
+sudo apt-get install zsh ctags openssh-server git-core bmon
 
 git submodule update --init --recursive
 
 ln -s ~/my_configs/.vim/.vimrc ~/.vimrc
 ln -s ~/my_configs/.vim/ ~/.vim
 ln -s ~/my_configs/.tmux.conf ~/.tmux.conf
-# install VIM plugins throught vundle
-vim +PluginInstall +qall
 
 echo Installing YouCompleteMe
 cd ~/my_configs/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 cd -
+
+# install VIM plugins throught vundle
+vim +PluginInstall +qall
+
 #ln -s ~/.my_configs/fontconfig/fonts.conf ~/.fonts.conf
 #ln -s ~/.my_configs/xfce4/ ~/.config/xfce4
 #ln -s ~/.my_configs/xfce4-session/ ~/.config/xfce4-session
@@ -24,3 +27,4 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 rm ~/.zshrc
 ln -s ~/my_configs/.zshrc ~/.zshrc
 
+./powerline_fonts/install.sh
